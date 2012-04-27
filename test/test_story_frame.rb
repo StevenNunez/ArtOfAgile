29103feb65444f04db67e6fd113493588641a0fb
Steven Nunez <steven.nunez@gmail.com>
2012-04-27 13:19:49 -0400

    [Steven] Added enumeration and prompts

--- test/test_story_frame.rb
+++ test/test_story_frame.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class TestStoryFrame < Test::Unit::TestCase
 
   def test_initialize
-    frame = StoryFrame({:question => "question1", :text => "text1",
+    frame = StoryFrame.new({:question => "question1", :text => "text1",
                          :choices => ["choice1","choice2"]})
     assert_equal "question1", frame.question
     assert_equal "text1", frame.text
