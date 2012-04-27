d8eb2f66fea6941e67994ded03087ebaa0a37866
Steven Nunez <steven.nunez@gmail.com>
2012-04-27 13:37:19 -0400

    [Steven & David] Enhanced user interface

--- lib/story_frame.rb
+++ lib/story_frame.rb
@@ -9,19 +9,28 @@ class StoryFrame
   end
 
   def display_text
+    system('clear')
+    print "\n########################################"
     puts @text
   end
 
   def ask_question_and_get_score
+    
     puts @question
+    puts "-" * 20
 
     @choices.each_with_index do |choice, index|
       puts "#{index +1}. #{choice[:text]}"
     end 
+    
+    print "-" * 20
+    puts ""
 
     response = gets.strip.to_i - 1
     
-    puts "#{@choices[response][:response]}"
+    puts "#{@choices[response][:response]}\n"
+    puts "Press Enter to continue"
+    gets
     return 0
   end
 
