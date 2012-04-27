ad1d3db4b9ff070dad461992128995c2e8d6d64f
Steven Nunez <steven.nunez@gmail.com>
2012-04-27 14:27:09 -0400

    [Najati & Steven] fixing spacing

--- lib/story_frame.rb
+++ lib/story_frame.rb
@@ -27,7 +27,9 @@ class StoryFrame
   def ask_question_and_get_score
     
     if @question
+      puts ""
       puts @question
+      puts ""
       puts "-" * 20
 
       display_choices
@@ -47,7 +49,9 @@ class StoryFrame
 
   def show_response(response)
     if @choices
+      puts ""
       puts "#{@choices[response][:response]}"
+      puts ""
     end
   end
 
